de2981eb4c0dd27c1df7e9d85f04b1eefc6f2f04
Oliver <35111378+SquidSushi@users.noreply.github.com>
2022-07-10 14:28:29 +0200

    we dont need this here actually

--- out/install/x64-Debug/game_Bundle/assets/graphics/Tilesets/Forest_Spirites_-_Testlevel.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.8" tiledversion="1.8.4" name="Forest Spirites - Testlevel" tilewidth="32" tileheight="32" tilecount="620" columns="20">
- <image source="../Tilesets/Tileset_Forest Spirites.png" width="640" height="992"/>
-</tileset>
