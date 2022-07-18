3fd844c944f648d9dd5383b2de83eb6f03614705
oliver.pruchnicki <35111378+SquidSushi@users.noreply.github.com>
2022-07-18 15:09:09 +0200

    some things were fucked up and crashing. They should not now

--- src/actor.cpp
+++ src/actor.cpp
@@ -3,7 +3,7 @@
 //
 #include "actor.h"
 
-void actor::drawActor(Texture2D texture, Vector2 pos)
+void actor::drawActor(Texture2D& texture, Vector2 pos)
 {
     this->actor_frames++;
     this->actor_frame_offset = actor_frames / frame_speed % (texture.width/32);
