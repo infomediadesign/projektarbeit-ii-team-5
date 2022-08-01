a483eef1de3bd27f85127c1d370610ddde11cd69
TricksterAky <100286052+TricksterAky@users.noreply.github.com>
2022-08-01 17:23:06 +0200

    level 1 Andy
    Erster Upload, texturen und kleine Level müssen noch geändert werden

---
+++ assets/graphics/Level_01/Ground.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.8" tiledversion="1.8.5" name="Ground" tilewidth="32" tileheight="32" tilecount="22528" columns="176">
+ <image source="Level_1.png" width="5632" height="4096"/>
+</tileset>
