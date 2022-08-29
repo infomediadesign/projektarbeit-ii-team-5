9cd762e26aad733835a11277eda6e67702b6557f
Lee E <98901977+KupferLee@users.noreply.github.com>
2022-08-29 12:00:37 +0200

    added slot selection and first row of slots

--- src/inventory.cpp
+++ src/inventory.cpp
@@ -5,14 +5,19 @@
 inventory::inventory(controlInput* x)
 {
     // inventory
+    this->scale_factor = 4;
 	this->texture = LoadTexture("assets/graphics/UI/Inventory/Inventory.png");
-	this->position.x = 0;
-	this->position.y = 0;
+	this->position.x = GetScreenWidth()/2 - texture.width*scale_factor/2;
+	this->position.y = GetScreenHeight()/2 - texture.height*scale_factor/2;
 
     this->bag_closed = LoadTexture("assets/graphics/UI/Inventory/BagClosed.png");
     this->bag_open = LoadTexture("assets/graphics/UI/Inventory/BagOpen.png");
+    this->selection = LoadTexture("assets/graphics/UI/Inventory/Slot_Selection.png");
 
     this->controlInputs = x;
+
+    this->setSlots();
+
     return;
 }
 
@@ -21,6 +26,7 @@ inventory::~inventory()
 	UnloadTexture(this->texture);
 	UnloadTexture(this->bag_closed);
 	UnloadTexture(this->bag_open);
+
 }
 
 void inventory::update()
@@ -31,6 +37,15 @@ void inventory::update()
 		this->isVisible = !this->isVisible;
 		std::cout << "Inventory bool has been flipped to: " << this->isVisible << std::endl;
 	}
+
+    if (IsKeyPressed(KEY_D) && current_slot < 3)
+    {
+        this->current_slot++;
+    }
+    else if (IsKeyPressed(KEY_A) && current_slot > 0)
+    {
+        this->current_slot--;
+    }
 }
 
 void inventory::draw()
@@ -44,16 +59,29 @@ void inventory::draw()
 
         DrawTexturePro(this->texture,
                        Rectangle{0, 0,(float) this->texture.width,(float) this->texture.height},
-                       Rectangle{(float)GetScreenWidth()/2 - texture.width*scaleFactor/2,  (float)GetScreenHeight()/2 - texture.height*scaleFactor/2,(float) this->texture.width*scaleFactor,(float) this->texture.height*scaleFactor},
+                       Rectangle{(float)position.x,  (float)position.y,(float) this->texture.width*scale_factor,(float) this->texture.height*scale_factor},
                        {0, 0}, 0.0, WHITE);
+
+        DrawTexturePro(this->selection,
+                       {0, 0, (float)this->selection.width, (float)this->selection.height},
+                       this->slot_positions[this->current_slot],
+                       {0, 0}, 0, WHITE);
 	}
 	else
 	{
         DrawTexturePro(this->bag_closed,
                        Rectangle{0, 0, (float)this->bag_open.width, (float)this->bag_open.height},
-                       Rectangle{(float)GetScreenWidth() - ((GetScreenWidth() -Game::ScreenWidth)/2) - 150,  (float)GetScreenHeight() - 150 , (float)(int)this->bag_open.width * scaleFactor, (float)this->bag_open.height * scaleFactor},
+                       Rectangle{(float)GetScreenWidth() - ((GetScreenWidth() -Game::ScreenWidth)/2) - 150,  (float)GetScreenHeight() - 150 , (float)(int)this->bag_open.width * 2, (float)this->bag_open.height * 2},
                        {0, 0}, 0.0, WHITE);
 	}
+}
+
+void inventory::setSlots()
+{
+    this->slot_positions[0] = {position.x + 16*scale_factor, position.y + 32*scale_factor, (float)selection.width*scale_factor, (float)selection.height*scale_factor};
+    this->slot_positions[1] = {slot_positions[0].x + this->slot_offset_x, slot_positions[0].y, (float)selection.width*scale_factor, (float)selection.height*scale_factor};
+    this->slot_positions[2] = {slot_positions[1].x + this->slot_offset_x, slot_positions[1].y, (float)selection.width*scale_factor, (float)selection.height*scale_factor};
+    this->slot_positions[3] = {slot_positions[2].x + this->slot_offset_x, slot_positions[2].y, (float)selection.width*scale_factor, (float)selection.height*scale_factor};
 }
 
 bool inventory::isActive() { return this->isVisible; }
--- src/inventory.h
+++ src/inventory.h
@@ -18,9 +18,15 @@ protected:
 	Image bag_img;
 	Texture2D bag_closed;
 	Texture2D bag_open;
+    Texture2D selection;
+    void setSlots();
+
+    Rectangle slot_positions[12];
 
 	controlInput* controlInputs{};
 	bool isVisible = false;
-    int scaleFactor = 4;
+    int slot_offset_x = (32 + 16) * scale_factor;
+    int slot_offset_y = 0;
+    int current_slot = 0;
 
 };
